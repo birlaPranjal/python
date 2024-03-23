31b3bd414cc64ff3afc92c53fe142ac0165c7563
pranjal <birlapranjal460@gmail.com>
2024-03-24 00:48:03 +0530

    second commit

---
+++ data types/02.py
@@ -0,0 +1,37 @@
+# Integer
+num = 10
+
+# Float
+pi = 3.14
+
+# String
+name = "John Doe"
+
+# Boolean
+is_true = True
+
+# List
+fruits = ["apple", "banana", "orange"]
+
+# Tuple
+coordinates = (10, 20)
+
+# Set
+numbers = {1, 2, 3, 4, 5}
+
+# Dictionary
+person = {"name": "John", "age": 30, "city": "New York"}
+
+# None
+empty_value = None
+
+# Printing the values
+print("Integer:", num)
+print("Float:", pi)
+print("String:", name)
+print("Boolean:", is_true)
+print("List:", fruits)
+print("Tuple:", coordinates)
+print("Set:", numbers)
+print("Dictionary:", person)
+print("None:", empty_value)
