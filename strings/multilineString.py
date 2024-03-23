6245b0dc245196af9b9ef0dce091d97c4b81bfca
pranjal <birlapranjal460@gmail.com>
2024-03-24 00:54:05 +0530

    multiline strings

---
+++ strings/multilineString.py
@@ -0,0 +1,13 @@
+# Multiline string
+multiline_string = '''This is a
+multiline
+string'''
+
+print(multiline_string)
+
+# Accessing string as an array
+string = "Hello, World!"
+print(string[0])  # Output: H
+print(string[7])  # Output: W
+print(string[-1])  # Output: !
+print(string[-2])  # Output: d
