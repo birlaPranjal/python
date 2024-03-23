0c49ac0efb6e8dacbf0d44911015cb2b33f3927a
pranjal <birlapranjal460@gmail.com>
2024-03-24 00:44:16 +0530

    first commit

---
+++ data types/01.py
@@ -0,0 +1,35 @@
+# Integer
+num = 10
+print("Integer:", num)
+
+# Float
+float_num = 3.14
+print("Float:", float_num)
+
+# String
+name = "GitHub Copilot"
+print("String:", name)
+
+# Boolean
+is_true = True
+print("Boolean:", is_true)
+
+# List
+my_list = [1, 2, 3, 4, 5]
+print("List:", my_list)
+
+# Tuple
+my_tuple = (1, 2, 3, 4, 5)
+print("Tuple:", my_tuple)
+
+# Set
+my_set = {1, 2, 3, 4, 5}
+print("Set:", my_set)
+
+# Dictionary
+my_dict = {"name": "Pranjal Birla", "age": 20}
+print("Dictionary:", my_dict)
+
+# None
+none_value = None
+print("None:", none_value)  
