c8fb4e18ff4f13009584a2c25093b54da2648389
pranjal <birlapranjal460@gmail.com>
2024-03-24 11:25:44 +0530

    renamed folders

--- 02_strings/01_string.py
+++ 02_strings/01_string.py
@@ -1,13 +1,16 @@
+# Single-line string
+single_line_string = "Hello, World!"
+print(single_line_string)
+
 # Multiline string
 multiline_string = '''This is a
 multiline
 string'''
-
 print(multiline_string)
 
 # Accessing string as an array
 string = "Hello, World!"
-print(string[0])  # Output: H
-print(string[7])  # Output: W
-print(string[-1])  # Output: !
-print(string[-2])  # Output: d
+print(string[0])    # Output: H
+print(string[7])    # Output: W
+print(string[-1])   # Output: !
+print(string[-2])   # Output: d
